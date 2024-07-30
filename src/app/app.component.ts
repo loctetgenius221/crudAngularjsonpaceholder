144400f0183cabdc222727d95a45bec19cc14be4
loctetgenius <sagnamoussa546@gmail.com>
2024-07-30 13:08:15 +0000

    Ajout des dépendances suivante: Bootstrap et HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'crudAngularjsonPlaceholder';
